d978078426882b72cf0f725e6ca29d66e8845871
Devjit Bedi <devjitbedi@Devjits-MacBook-Pro.local>
2019-05-18 00:06:48 -0700

    for mobile

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <head>
   <meta charset="utf-8">
   <title>@yield('title')</title>
